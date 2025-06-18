08a6a9e181666febdbfc3ffd9893f1c799431b5d
Cyrus <markcyrus.serrano@gmail.com>
2025-06-18 18:08:26 +0800

    Homepage v1

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,8 +6,8 @@ function Button({
   className,
   type,
 }: {
-  title: String;
-  className: String;
+  title: string;
+  className: string;
   type?: "button" | "submit" | "reset";
 }) {
   return (
